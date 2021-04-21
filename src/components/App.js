999446b9f310d2c09bf1f0a305529937a9849030
Elodie Lefebvre <elodielefebvre@Elodies-MacBook-Pro.local>
2021-04-21 16:22:19 -0400

    initial setup

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import GlobalStyles from "../GlobalStyles";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Take it one day at the time</header>
+      <GlobalStyles />
+    </div>
+  );
+}
+
+export default App;
